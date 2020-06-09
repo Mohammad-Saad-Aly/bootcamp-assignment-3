402e1e56f2253ac9ebfc59d67a1b51c3af2d0a02
Mohammad Saad Ali <mohammadsaadali786.92@gmail.com>
2020-06-10 00:04:26 +0500

    Update assignment1.js

--- src/assignment1.js
+++ src/assignment1.js
@@ -4,7 +4,7 @@ import Assignment2 from './assignment2';
 function Assignment(props) {
     return (
         <div>
-            <h1>Assignment Hello {props.No} by {props.sirName} </h1>
+            <h1>Assignment {props.No} by {props.sirName} </h1>
             <Assignment2 no="2" sirName="Mohsin Khalid" />
         </div>
     )
